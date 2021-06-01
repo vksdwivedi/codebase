d7ed7fdf893e94ed9ab40ea2b910eeafd8bcf10d
vksdwivedi <vks.dwivedi9@gmail.com>
2021-06-01 16:58:58 +0530

    indentation update

--- python/main.py
+++ python/main.py
@@ -20,5 +20,3 @@ if __name__ == '__main__':
 
     inverse = [(key, value) for key, value in my_dict.items()]
     print(max(inverse))
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
